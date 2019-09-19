b56d314797cd456686c8e0a20a6980fc613dc51f
rakesh.k <rakesh.k@pacewisdom.com>
2019-09-19 14:43:42 +0530

    token valdation added

--- services/email.service.js
+++ services/email.service.js
@@ -27,42 +27,54 @@ async function sendEmail(req) {
     return new Promise(async (resolve, reject) => {
         try {
 
-            let testAccount = await nodemailer.createTestAccount();
-            let transporter = nodemailer.createTransport({
-                host: config.email_config.smtp_host,
-                port: config.email_config.smtp_port,
-                secure: false, // true for 465, false for other ports
-            });
+            // console.log("req.headers['x-auth-token']",req.headers['x-auth-token']);
 
-            req.body.emails.forEach(function (ele) {
-                // send mail with defined transport object
-                let info = transporter.sendMail({
-                    from: '<'+ config.email_config.from_email_id+'>', // sender address
-                    to: ele, // list of receivers
-                    subject: req.body.subject, // Subject line
-                    text: req.body.message // plain text body
-                    // html: '<b>Hello world?</b>' // html body
+            if (req.headers['x-auth-token'] && req.headers['x-auth-token'] == config.token) {
+
+                let testAccount = await nodemailer.createTestAccount();
+                let transporter = nodemailer.createTransport({
+                    host: config.email_config.smtp_host,
+                    port: config.email_config.smtp_port,
+                    secure: false, // true for 465, false for other ports
                 });
 
-                winston.log(info);
+                req.body.emails.forEach(function (ele) {
+                    // send mail with defined transport object
+                    let info = transporter.sendMail({
+                        from: '<' + config.email_config.from_email_id + '>', // sender address
+                        to: ele, // list of receivers
+                        subject: req.body.subject, // Subject line
+                        text: req.body.message // plain text body
+                        // html: '<b>Hello world?</b>' // html body
+                    });
 
-            });
+                    winston.log(info);
+
+                });
 
 
 
-            // console.log('Message sent: %s', info.messageId);
-            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+                // console.log('Message sent: %s', info.messageId);
+                // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-            // Preview only available when sending through an Ethereal account
-            // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+                // Preview only available when sending through an Ethereal account
+                // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+                // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
 
-            // main().catch(console.error);
-            return resolve({
-                status: "success",
-                message: "successfully mail sent",
-            })
+                // main().catch(console.error);
+                return resolve({
+                    status: "success",
+                    message: "successfully mail sent",
+                })
+
+            } else {
+                return resolve({
+                    status: "failed",
+                    message: "invalid token ",
+                })
+            }
+
         } catch (error) {
             return reject({
                 status: "failed",
